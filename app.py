13b16a7b372163b7a96200b1ff9caf00e36de898
josselcardozo <jm.cardozoj@uea.edu.ec>
2026-02-12 00:24:14 -0500

    Fix puerto 0.0.0.0 para Render

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+# Ruta principal
 @app.route('/')
 def inicio():
-    return "Bienvenido a Biblioteca Virtual – Consulta de libros disponibles"
+    return "Bienvenido a Biblioteca Virtual – Sistema desarrollado por Jossel 🚀"
 
+# Ruta dinámica para libros
 @app.route('/libro/<titulo>')
 def libro(titulo):
-    return f"Libro: {titulo} – consulta exitosa."
+    return f"Libro: {titulo} – consulta exitosa en el sistema de Jossel."
 
+# Ruta dinámica para usuario
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f"Bienvenido, {nombre}. Tu sesión en la Biblioteca está activa."
+    return f"Bienvenido, {nombre}. Tu sesión en la Biblioteca está activa. – Proyecto de Jossel"
 
-print(">>> Aplicación Flask iniciando...")
 
-app.run(host="127.0.0.1", port=5000, debug=True)
+# SOLO para ejecución local
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
